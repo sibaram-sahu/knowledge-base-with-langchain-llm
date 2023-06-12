abf9e5eafa169244d7783ed994cc4ac91572b433
sibaspage <13863225+sibaspage@users.noreply.github.com>
2023-06-12 16:39:11 +0530

    Application ready

--- app.py
+++ app.py
@@ -1,16 +1,114 @@
 import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplate import css, bot_template, user_template
+from langchain.llms import HuggingFaceHub
 
+def get_pdf_text(pdf_docs):
+    """ 
+        extract text from the pdfs
+    """
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+    return text;
+
+def get_text_chunks(texts):
+    '''
+        get the texts from pdf and chunk it
+    '''
+    text_splitter = CharacterTextSplitter(
+        separator = "\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(texts)
+    return chunks
+
+def get_vector_store(text_chunks):
+    """
+        convert chunks to vector
+    """
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    # embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_texts(text_chunks, embedding=embeddings)
+    return vectorstore
+
+
+def get_conversation_chain(vectorstore):
+    """
+        Get conversation from vector store
+    """
+    # llm = ChatOpenAI()
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+def handle_userinput(user_question):
+    """
+        Question and answer user inputs
+    """
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
 
 def main():
+    load_dotenv()
     st.set_page_config(page_title="Conversation with Knowledge Base", page_icon=":books:")
+    st.write(css, unsafe_allow_html=True)
+
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
 
     st.header("Conversation with Knowledge Base :books:")
-    st.text_input("Write a query about your documents.")
+    user_question = st.text_input("Ask a query about your documents:")
+    if user_question:
+        handle_userinput(user_question)
+
 
     with st.sidebar:
         st.subheader('Knowledge Base')
-        st.file_uploader("Upload your Knowledge Base(pdfs)")
-        st.button("Upload")
+        pdf_docs = st.file_uploader("Upload your Knowledge Base(pdfs)", accept_multiple_files=True)
+        
+        if st.button("Upload"):
+            with st.spinner("Uploading..."):
+                # Get the text from pdf
+                raw_text = get_pdf_text(pdf_docs)
+                # Get the text chunks
+                text_chunks = get_text_chunks(raw_text)
+
+                # Store in vector format
+                vector_store = get_vector_store(text_chunks)
+
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(vector_store)
 
 if __name__ == "__main__":
     main()
